8d091f93b9cd3e511c7b2641babfcbc070aaa584
GQ722 <deveshamin7@gmail.com>
2016-06-07 13:48:05 -0400

    Chapter1: Problem1 and Problem2

---
+++ IntoToJavaProgramming/src/chapter1/problem1.java
@@ -0,0 +1,28 @@
+/**
+ * Problem: (Display three messages) Write a program that displays 'Welcome to Java,
+ * Welcome to Computer Science, and Programming is Fun.
+ * 
+ */
+
+package chapter1;
+
+//creating class having the same name as the program name
+public class problem1 { 
+
+	//main class
+	public static void main(String[] args) {
+		
+		// print a String that is enlosed in " "
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Computer Science");
+		System.out.println("Programming is fun");
+		
+	} //closing the main method
+} //closing the class
+
+/**
+* Output:
+* Welcome to Java
+* Welcome to Computer Science
+* Programming is fun
+ */
---
+++ IntoToJavaProgramming/src/chapter1/problem2.java
@@ -0,0 +1,32 @@
+/**
+ * Problem: (Display five messages) Write a program that displays Welcome to Java five times.
+ */
+
+package chapter1;
+
+//creating class having the same name as the program name
+public class problem2 {
+
+	//main class
+	public static void main(String[] args) {
+
+		// print a String that is enclosed in " "
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		System.out.println("Welcome to Java");
+		
+		/**
+		 * The more efficient way to write the same problem is the following
+		 * This method uses 'for loop', if you are not familiar with 'for loop'
+		 * stop here and use the above beginner code. You will learn the concept
+		 * of loops in the later chapters.
+		 */
+		
+//		for(int i=0; i<5; i++) {
+//			System.out.println("Welcome to Java");
+//		}
+		
+	} //end of main
+} //end of class
