5bdc280cbebfa2c347ba83ae9ab1afd62e1911fb
GQ722 <deveshamin7@gmail.com>
2016-06-08 07:33:57 -0400

    Chapter1 Problem3

---
+++ IntoToJavaProgramming/src/chapter1/Problem3.java
@@ -0,0 +1,26 @@
+/**
+ * Problem: (Display a pattern) Write a program that displays the following pattern:
+ * 
+ *     J      A     V     V      A
+ *     J     A A     V   V      A A
+ * J   J    AAAAA     V V      AAAAA
+ *  J J    A     A     V      A     A
+ *  
+ */
+
+package chapter1;
+
+//creating a class with the same name as the program name
+public class Problem3 {
+
+	//main method
+	public static void main(String[] args) {
+		
+		//print statements
+		System.out.println(      "\tJ"+      "\tA"    +       "\tV     V"+"      A"   );
+		System.out.println(      "\tJ"+ "      A A"   +   "       V   V" +"      A A"  );
+		System.out.println("    J   J"+ "     AAAAA"  +   "       V V"  +"      AAAAA" );
+		System.out.println("     J J"+ "     A     A" +   "       V"   +"      A     A");
+	
+	} //end of main
+} //end of class
