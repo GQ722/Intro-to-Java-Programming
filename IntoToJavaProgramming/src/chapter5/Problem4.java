4dabbc310fdebed81592a3d0a9fce7a3e9b8d3e9
GQ722 <amin.17@wright.edu>
2016-06-10 11:42:18 -0400

    Finished chapter5 Problem4

---
+++ IntoToJavaProgramming/src/chapter5/Problem4.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package chapter5;
+
+/**
+ * @author Devesh Patel
+ *
+ */
+public class Problem4 {
+
+	/**
+	 * main method
+	 */
+	public static void main(String[] args) {
+		
+		//print the titles
+		System.out.println("Miles        Kilometers");
+		
+		for(int i=1; i<=100; i+=1) {
+			System.out.println(i + "\t\t" + i*1.609);
+		}
+	}
+}
